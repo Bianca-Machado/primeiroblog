95037af6e97d295caefb24aec286a3004fddabf1
Bianca-machado <80485783+Bianca-Machado@users.noreply.github.com>
2021-06-14 03:35:08 -0300

    Criacao de modelo de postagem

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = models.TextFiels()
-    created_date = models.DateTimeField(blank=True, null=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
